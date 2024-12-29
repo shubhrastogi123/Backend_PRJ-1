8b749f24f3915213b317f878128fed769fe3ad80
Shubham <rastogishubham465@gmail.com>
2024-12-29 15:32:39 +0530

    user, video models- jwt, bcrypt, mongooseAggregate

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,7 +1,7 @@
 //Method-1
 
 const asyncHandler = (fn) => {
-    (req, res, next) => {
+    return (req, res, next) => {
         Promise.resolve(fn(req, res, next))
         .catch((err) => next(err))
     }
